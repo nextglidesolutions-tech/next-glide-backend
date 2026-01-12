bdeb84575718e94ecba5070e7fdc4e6a32cdeffc
Sanjay <attellisanjay29@gmail.com>
2026-01-12 11:53:53 +0530

    toml added1

--- server.js
+++ server.js
@@ -16,7 +16,26 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors());
+// CORS Configuration
+const allowedOrigins = [
+    'https://next-glide.netlify.app',
+    'http://localhost:5173',
+    'http://localhost:8080',
+    'http://localhost:3000'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps, curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
+}));
 app.use(express.json());
 app.use((req, res, next) => {
     console.log('Body:', JSON.stringify(req.body, null, 2));
